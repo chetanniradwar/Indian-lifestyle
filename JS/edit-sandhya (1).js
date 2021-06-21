6f7b63e857fcb5e0c825aaaa8502b2347fe301a4
chetannirdwar <62559514+chetannirdwar@users.noreply.github.com>
2021-06-21 12:18:26 +0530

    added images and updated map.js

---
+++ JS/edit-sandhya (1).js
@@ -0,0 +1,253 @@
+switch (this.id) {
+    case "IN-AN":
+        cardImg.src = "../media/Andaman_Nicobar.jpg"
+        cardHead.innerHTML = "Andaman-Nicobar"
+        cardSummary.innerHTML = "Andaman and Nicobar Islands, an archipelago territory of India, is widely famous for a cornucopia of tropical destinations and tourist activities. These islands are known for many things from beach holidays to trekking to marine wildlife and sea adventures."
+        // console.log("hello")
+        break;
+    case "IN-AP":
+        cardImg.src = "../media/Andra_Pradesh.jpg"
+        cardHead.innerHTML = "Andra Pradesh"
+        cardSummary.innerHTML = "The state draws it's name from the Andra people,who have inhabited the area since antiquity and developed their own language,Telugu.One of the speciality of the state is that there is a Venkateswara Temple at Tirupati is the world's second-richest temple."
+        //console.log("hello")
+        break;
+    case "IN-AR":
+        cardImg.src = "../media/Arunachal_Pradesh.jpg"
+        cardHead.innerHTML = "Arunachal Pradesh"
+        cardSummary.innerHTML = "Arunachal Pradesh constitutes a mountainous area in the extreme northeastern part of the country.It means Land of the Dawn-Lit Mountains, which is the sobriquet for the state in Sanskrit. The state is also known as the Orchid State of India or the Paradise of the Botanists. "
+        //console.log("hello")
+        break;
+    case "IN-AS":
+        cardImg.src = "../media/Aasam.jpg"
+        cardHead.innerHTML = "Aasam"
+        cardSummary.innerHTML = "The name is derived from the word 'asama' meaning 'peerless' in the now extinct Ahom language.It is known for Assam tea and Assam silk.The state was the first site for oil drilling in Asia.It is home to the one-horned Indian rhinoceros, provides one of the last wild habitats for the Asian elephant."
+        //console.log("hello")
+        break;
+    case "IN-BR":
+        cardImg.src = "../media/Bihar.jpg"
+        cardHead.innerHTML = "Bihar"
+        cardSummary.innerHTML = "Derivation of the name is from the Sanskrit vihara(Buddhist manastery) reflects the prominence of such communities in the region in ancient times.Bihar is the place from where the concept of non-violence originated, the most enchanting thought in the history of mankind.It is also the birthplace of two religions Jainism and Buddhism."
+        //console.log("hello")
+        break;
+    case "IN-CH":
+        cardImg.src = "../media/Chandigarh.jpg"
+        cardHead.innerHTML = "Chandigarh"
+        cardSummary.innerHTML = "The word Chandigarh literally means Chandi Garh or Fort of Goddess Chandi – a name derived from a temple situated nearby in Panchakula.It is also called the 'city beautiful' ,is a Union Territory and capital of two states Punjab & Haryana. "
+        //console.log("hello")
+        break;
+    case "IN-CT":
+        cardImg.src = "../media/Chhatisgarh.jpg"
+        cardHead.innerHTML = "Chhatisgarh"
+        cardSummary.innerHTML = "It is situated in the heart of India, contains the source of one of the most important rivers of the South Asian penisula-the Mahanadi.The State is full of ancient monuments, rare wildlife species, carved temples, Buddhist sites, palaces, water-falls, caves, rock paintings and hill plateaus." 
+        //console.log("hello")
+        break;
+    case "IN-DD":
+        cardImg.src = "../media/Daman_and_Diu.jpg"
+        cardHead.innerHTML = "Daman And Diu"
+        cardSummary.innerHTML = "Daman is a famous for its beach, Portuguese colonial architecture, churches, and for the scenic beauty in the twin towns of Nani-Daman and Moti-Daman.It is famous for its leather products.\n Diu is Famous for its pristine beauty and swaying palm trees, The serene beauty of the beach due to its white sands and quiet blue water of Nagoa and many ther beaches in Diu is an ideal place for tourists all over the world."
+        //console.log("hello")
+        break;
+    case "IN-DL":
+        cardImg.src = "../media/Delhi.jpg"
+        cardHead.innerHTML = "Delhi"
+        cardSummary.innerHTML = "The distinguished city 'Delhi' is blessed with several gardens and botanical houses.It is of great historical significance as an important commercial, transport, and cultural hub, as well as the political centre of India."
+        //console.log("hello")
+        break;
+    case "IN-DN":
+        cardImg.src = "../media/Dadra_And_Nagar.jpg"
+        cardHead.innerHTML = "Dadra And Nagar Haveli"
+        cardSummary.innerHTML = "Dadra and Nagar Haveli is one of the Union Territories of India, and it is located between Maharashtra and Gujarat.The unmatched beauty of Dadra and Nagar Haveli is dotted distant hills, lush greenery, cascading waterfalls, rich culture and Portuguese architecture."
+        //console.log("hello")
+        break;
+    case "IN-GA":
+        cardImg.src = "../media/Goa.jpg"
+        cardHead.innerHTML = "Goa"
+        cardSummary.innerHTML = "That Goa is the smallest state of India.Goa's official language, Konkani, is the only Indian language that is written in two ,Devnagri and Roman English scripts.Most famous among them are Goa Sausages, a spicy chorizo that's available all over India, but best made in Goa. Then there are cashew nuts, which are of great quality and much cheaper, too."
+        //console.log("hello")
+        break;
+    case "IN-GJ":
+        cardImg.src = "../media/Gujarat.jpg"
+        cardHead.innerHTML = "Gujarat"
+        cardSummary.innerHTML = "Gujarat is derived from Sanskrit (Gurjar-Rashtra), the Gurjar nation.Gujarat is famous for its traditional clothing, food and natural landscape. Asiatic lions, colourful handicrafts, festival and culture are some of the things that make Gujarat famous."
+        //console.log("hello")
+        break;
+    case "IN-HP":
+        cardImg.src = "../media/Himachal_Pradesh.jpg"
+        cardHead.innerHTML = "Himachal Pradesh"
+        cardSummary.innerHTML = "Himachal Pradesh is home to the largest glacier in Asia,The Shigri Glacier.The Manali – Leh National Highway is famed for being the highest motor able road in the world.Many outdoor activities such as rock climbing, mountain biking, trekking, rafting, and heli-skiing are popular tourist attractions."
+        //console.log("hello")
+        break;
+    case "IN-HR":
+        cardImg.src = "../media/Haryana.jpg"
+        cardHead.innerHTML = "Haryana"
+        cardSummary.innerHTML = "Haryana is renowned for its ancient temples, impressive architecture and lush greenery.The rich Haryanvi culture is characterised by the hookahs and the charpoys, the vivid fairs and the swaying paddy fields.It is one of the most economically developed regions in South Asia."
+        //console.log("hello")
+        break;
+    case "IN-JH":
+        cardImg.src = "../media/Jharkhand.jpg"
+        cardHead.innerHTML = "Jharkhand"
+        cardSummary.innerHTML = " Jharkhand is known for its vast forest resources.During the Mughal period, the region, then known as Khukhra, was famous for its diamonds.Jharkhand is one of the richest mineral zones in the world and boasts."
+        //console.log("hello")
+        break;
+    case "IN-JK":
+        cardImg.src = "../media/Jammu_Kashmir.jpg"
+        cardHead.innerHTML = "Jammu And Kashmir"
+        cardSummary.innerHTML = "Popularly known as the 'Paradise on Earth', Jammu and Kashmir is world famous for its scenic splendour, snow-capped mountains, plentiful wildlife, exquisite monuments, hospitable people and local handicrafts."
+        //console.log("hello")
+        break;
+    case "IN-KA":
+        cardImg.src = "../media/Karnataka.jpg"
+        cardHead.innerHTML = "Karnataka"
+        cardSummary.innerHTML = "Karnataka, is the silk hub of India and Karnataka silk sarees are its traditional costume.The culture of Karnataka revolves around its dance, music folk art and drama, and literature.It is a treasure house of ritualistic dances."
+        //console.log("hello")
+        break;
+    case "IN-KL":
+        cardImg.src = "../media/Kerala.jpg"
+        cardHead.innerHTML = "Kerala"
+        cardSummary.innerHTML = "Kerala is well-known for its mesmerizing backwaters, ecotourism initiatives, scenic beauty, amazing with coconut-lined sandy beaches, and mouthwatering cuisine."
+        //console.log("hello")
+        break;
+    case "IN-LD":
+        cardImg.src = "../media/Lakshadweep.jpg"
+        cardHead.innerHTML = "Lakshadweep"
+        cardSummary.innerHTML = "India's smallest Union Territory Lakshadweep is an archipelago consisting of 36 islands is known for its exotic and sun-kissed beaches and lush green landscape.It is typically famous for its Kerala cuisines."
+        //console.log("hello")
+        break;
+    case "IN-MH":
+        cardImg.src = "../media/Maharashtra11.jpg"
+        cardHead.innerHTML = "Maharashtra"
+        cardSummary.innerHTML = "Maharashtra is famous for its caves, and rock-cut architecture.The most-popular food forms are bhaji, vada pav, misalpav and pav bhaji. More-traditional dishes are sabudana khichadi, pohe, upma, sheera and panipuri. Most Marathi fast food and snacks are lacto-vegetarian."
+        //console.log("hello")
+        break;
+    case "IN-ML":
+        cardImg.src = "../media/Meghalaya.jpg"
+        cardHead.innerHTML = "Meghalaya"
+        cardSummary.innerHTML = "Meghalaya is the home to a large variety of fruits, vegetables, spices, and medicinal plants. Meghalaya is also famous for its large variety of orchids. Famous for its high rainfall, subtropical forests and biodiversity."
+        //console.log("hello")
+        break;
+    case "IN-MN":
+        cardImg.src = "../media/Manipur.jpg"
+        cardHead.innerHTML = "Manipur"
+        cardSummary.innerHTML = "It ia known as 'Jewel Land', apart from that,it is the birthplace of Polo, the sport and the birthplace of Ras Lila, a classical dance form.Home to the famous Sangai,a rare species of deer and Siroy or Shirui Lily."
+        //console.log("hello")
+        break;
+    case "IN-MP":
+        cardImg.src = "../media/Madhya_Pradesh.jpg"
+        cardHead.innerHTML = "Madhya Pradesh"
+        cardSummary.innerHTML = "It is the 'Heart of India', as well the second largest state in India.It is particularly famous for its Khajuraho temples which exhibit the erotic sculptures depicting the ancient India Kama Sutra text."
+        //console.log("hello")
+        break;
+    case "IN-MZ":
+        cardImg.src = "../media/Mizoram.jpg"
+        cardHead.innerHTML = "Mizoram"
+        cardSummary.innerHTML = "Known for its evergreen hills and dense bamboo jungles,Called the Land of Blue Mountains."
+        //console.log("hello")
+        break;
+    case "IN-NL":
+        cardImg.src = "../media/Nagaland.jpg"
+        cardHead.innerHTML = "Nagaland"
+        cardSummary.innerHTML = " it is the home to the hottest chilli peppers in the world. Nagaland is known as 'the only predominantly Baptist state in the world' and 'the most Baptist state in the world.' "
+        //console.log("hello")
+        break;
+    case "IN-OR":
+        cardImg.src = "../media/Orrisa.jpg"
+        cardHead.innerHTML = "Orrisa or Odisa"
+        cardSummary.innerHTML = "Odisha is famous for its heritage sites and enthralling views.Moreover, Puri Beach Festival and the Jagannath Rath Yatra attracts tons of tourists to Odisha yearly."
+        //console.log("hello")
+        break;
+    case "IN-PB":
+        cardImg.src = "../media/Panjab.jpg"
+        cardHead.innerHTML = "Panjab"
+        cardSummary.innerHTML ="Punjab is referred to as 'The land of five rivers'.It is also famous for its religious diversity as it was here many religious movements were initiated.Lohri can be said to be the most important festival of Punjab."
+        //console.log("hello")
+        break;
+    case "IN-PY":
+        cardImg.src = "../media/Puducherry.jpg"
+        cardHead.innerHTML = "Puducherry"
+        cardSummary.innerHTML = "Often referred to as the 'French Capital of India', Pondicherry is a vibrant city brimming with unspoiled beaches, heritage buildings, French and Tamil quarters, ancient temples and churches and a colonial ambience."
+        //console.log("hello")
+        break;
+    case "IN-RJ":
+        cardImg.src = "../media/Rajastan.jpg"
+        cardHead.innerHTML = "Rajastan"
+        cardSummary.innerHTML = "Rajasthan is known for its historical hill forts & palaces such as,Umaid Bhawan Palace (one of the largest private residences in the world).It has the largest desert of India-Thar Desert(The Great Indian Desert)."
+        //console.log("hello")
+        break;
+    case "IN-SK":
+        cardImg.src = "../media/Sikkim.jpg"
+        cardHead.innerHTML = "Sikkim"
+        cardSummary.innerHTML = "The state is famous for dazzling waterfalls, virgin forests, Tibetan style Buddhist Gompas, alpine meadows, rhododendron flowers and more.ikkim Mandarin represents the most important commercial fruit of Sikkim."
+        //console.log("hello")
+        break;
+    case "IN-TG":
+        cardImg.src = "../media/Telangana.jpg"
+        cardHead.innerHTML = "Telangana"
+        cardSummary.innerHTML = "Unique multi-cultural region where two different cultures coexist together, thus making Telangana the representative of the Deccan Plateau and its heritage with Warangal and Hyderabad being its epicenter."
+        //console.log("hello")
+        break;
+    case "IN-TN":
+        cardImg.src = "../media/Tamil_Nadu.jpg"
+        cardHead.innerHTML = "Tamil Nadu"
+        cardSummary.innerHTML = "Tamil Nadu is popular itself for the beautiful beaches, the magnificent temples, picturesque wildlife sanctuaries, colourful festivals and a plethora of panoramic sites."
+        //console.log("hello")
+        break;
+    case "IN-TR":
+        cardImg.src = "../media/Tripura.jpg"
+        cardHead.innerHTML = "Tripura"
+        cardSummary.innerHTML = "Tripura is famous for its cultivations, tea plantations, beautiful gardens, toy trains and much more. Being known for its breathtaking scenic view, this state is also popular for its tribe culture and food."
+        //console.log("hello")
+        break;
+    case "IN-UT":
+        cardImg.src = "../media/Uttarakhand.jpg"
+        cardHead.innerHTML = "Uttaranchal"
+        cardSummary.innerHTML = "Simply Heaven Uttarakhand is known for its natural beauty of the composed Himalayas, holy rivers, spiritual centres, and spellbinding landscapes,famous for adventure sports, an enrapturing variety of flora and fauna."
+        //console.log("hello")
+        break;
+    case "IN-UP":
+        cardImg.src = "../media/Uttar_Pradesh.jpg"
+        cardHead.innerHTML = "Uttar Pradesh"
+        cardSummary.innerHTML = "Uttar Pradesh is known for it's rich culture and tradition. It is home to Ayodhya and Mathura birthplace of Lord Rama.Taj Mahal, one of the New Seven Wonders of the World in Agra is also located in Uttar Pradesh."
+        //console.log("hello")
+        break;
+    case "IN-WB":
+        cardImg.src = "../media/West_Bengal.jpg"
+        cardHead.innerHTML = "West Bengal"
+        cardSummary.innerHTML = "West Bengal is famous for its terracotta temples of Bishnupur. The most popular festival celebrated in West Bengal is Durga Puja where all the people come out in the streets and celebrate this four day festival."
+        //console.log("hello")
+        break;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+}
+
+
+
+function hideCardData(tag) {
+var tooltipTag = document.getElementById("tooltip-span")
+tooltipTag.style.display = "none";
+
+if (tag.id == "IN-AN") {
+
+}
+}
+
+var iframe =document.getElementById("iframe-div")
+var svg2=document.getElementById("svg2")
+$("path").click(dothis)
+function dothis()
+{
+
+var videoTag= document.getElementById("play-video")
+iframe.style.display="block"
+
+svg2.style.display='none'
+// var body=document.getElementsByTagName("body")
+// body.style.background="rgba(0, 0, 0, 0.582)"
+
+switch (this.id) {
+case "IN-AN":
+    videoTag.src ="https://www.youtube.com/embed/h16HA4XjyD8"
+    break;
+case "IN-AP":
+    videoTag.src ="https://www.youtube.com/embed/_e01TCaGsQg"
+    break;
+}
+}
---
+++ JS/edit-sandhya.js
@@ -0,0 +1,109 @@
+switch (this.id) {
+    case "IN-AN":
+        cardImg.src = "../media/indianlifestyle.jpg"
+        cardHead.innerHTML = "Andanman Nicobar"
+        cardSummary.innerHTML = "Hi hello"
+        // console.log("hello")
+        break;
+    case "IN-AP":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-AR":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-AD":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-BR":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-CH":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-CT":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-DD":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-AN":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-AN":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-MH":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+    case "IN-AN":
+        cardImg.src = ""
+        cardHead.innerHTML = "fj"
+        cardSummary.innerHTML = "chetan"
+        console.log("hello")
+        break;
+
+}
+
+}
+
+function hideCardData(tag) {
+var tooltipTag = document.getElementById("tooltip-span")
+tooltipTag.style.display = "none";
+
+if (tag.id == "IN-AN") {
+
+}
+}
+
+var iframe =document.getElementById("iframe-div")
+var svg2=document.getElementById("svg2")
+$("path").click(dothis)
+function dothis()
+{
+
+var videoTag= document.getElementById("play-video")
+iframe.style.display="block"
+
+svg2.style.display='none'
+// var body=document.getElementsByTagName("body")
+// body.style.background="rgba(0, 0, 0, 0.582)"
+
+switch (this.id) {
+case "IN-AN":
+    videoTag.src ="https://www.youtube.com/embed/h16HA4XjyD8"
+    break;
+case "IN-AP":
+    videoTag.src ="https://www.youtube.com/embed/_e01TCaGsQg"
+    break;
+}
+}
